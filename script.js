bbe43ec9b37065e7ba7ef2879d3648c04941341c
BrahamjeetRandhawa <152489943+Braham3030@users.noreply.github.com>
2026-03-31 12:14:06 +0200

    Changes made for files

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
 
+
 console.log("Hello World!");
-
-const testToggle = document.getElementById("summarizeCheckbox");
-
-testToggle.addEventListener("change", async () => {
-
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        args: [testToggle.checked],
-        func: (isChecked) => {
-            if (isChecked) {
-                document.body.style.backgroundColor = "red";
-            } else {
-                document.body.style.backgroundColor = "";
-            }
-        }
-    })
-
-})
