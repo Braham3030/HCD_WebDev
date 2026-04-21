ead5b4cd080d6e493ed6707f0491fa37aa231fbc
BrahamjeetRandhawa <152489943+Braham3030@users.noreply.github.com>
2026-04-21 09:24:49 +0200

    Update API key and voice ID in configuration for improved functionality

--- env.js
+++ env.js
@@ -1,4 +1,11 @@
+// const config = {
+//     elevenLabsApiKey: 'sk_cca172330cc003906f0e075a5db099dcb93860f85ace7865',
+//     elevenLabsVoiceId: 'H1cbJ0RoDhBMT5gvm5vi',
+// }
+
 const config = {
-    elevenLabsApiKey: 'sk_cca172330cc003906f0e075a5db099dcb93860f85ace7865',
-    elevenLabsVoiceId: 'H1cbJ0RoDhBMT5gvm5vi',
+    elevenLabsApiKey: 'sk_bac6cbf758700086f7b8016d2c103205e0acc193c9bf5e12',
+    elevenLabsVoiceId: 'vxkjRthiHdYLogGcpy0G',
 }
+
+
